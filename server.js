41861bfd8d42c12759214c361890e994f202c4f1
gigantes88 <gigantes88@gmail.com>
2017-08-30 15:26:58 +0900

    로그인창 설정

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // express 사용요청
+const morgan = require('morgan'); // morgan 사용요청
+const basicAuth = require('express-basic-auth') // express-basic-auth 사용요청
+const app = express();  // express 사용 초기화
 
-app.set('view engine', 'ejs');
-app.use('/static', express.static('public'));
+app.set('view engine', 'ejs');  // ejs 설정
+app.use('/static', express.static('public')); // static 설정
+app.use(morgan('tiny'));  // morgan 설정
+app.use(basicAuth({ // basicAuth 설정
+  users: { 'admin': 'admin' },
+  challenge: true,
+  realm: 'Imb4T3st4pp'
+}))
 
 app.get('/', (req, res) => {
   res.render('index');

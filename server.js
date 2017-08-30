5e80e071403fe14d1eda7aed4899c43ffef4957c
gigantes88 <gigantes88@gmail.com>
2017-08-30 15:47:40 +0900

    randomstring 설정

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require('express'); // express 사용요청
-const morgan = require('morgan'); // morgan 사용요청
-const basicAuth = require('express-basic-auth') // express-basic-auth 사용요청
-const app = express();  // express 사용 초기화
+const express = require('express'); // 1. express 사용요청 
+const app = express();  // 2. express 사용 초기화
+const morgan = require('morgan'); // 3. morgan 사용요청
+const basicAuth = require('express-basic-auth') // 4. express-basic-auth 사용요청
+const randomstring = require("randomstring"); // 5. 랜덤문자생성 사용 요청
 
 const data = [
-  {longUrl: 'http://google.com', id: '189dsls38'}
+  {longUrl: 'http://google.com', id: randomstring.generate(12)}  // id에 randomstring을 생성
 ];
-// http://localhost:3000/38nzei39
-// 302 응답
-// 
 
 app.set('view engine', 'ejs');  // ejs 설정
 app.use('/static', express.static('public')); // static 설정

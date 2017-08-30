eb6ecc3b796f4d1a924c645466ccca1f51f64f4a
gigantes88 <gigantes88@gmail.com>
2017-08-30 15:42:43 +0900

    fake data 생성

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const morgan = require('morgan'); // morgan 사용요청
 const basicAuth = require('express-basic-auth') // express-basic-auth 사용요청
 const app = express();  // express 사용 초기화
 
+const data = [
+  {longUrl: 'http://google.com', id: '189dsls38'}
+];
+// http://localhost:3000/38nzei39
+// 302 응답
+// 
+
 app.set('view engine', 'ejs');  // ejs 설정
 app.use('/static', express.static('public')); // static 설정
 app.use(morgan('tiny'));  // morgan 설정
@@ -13,7 +20,7 @@ app.use(basicAuth({ // basicAuth 설정
 }))
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {data});
 });
 
 app.listen(3000, () => {
